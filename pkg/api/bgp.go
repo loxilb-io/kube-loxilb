883216fda133b4368c54363318d594b0877ca024
Trekkie Coder <trekkie@netlox.io>
2023-07-20 18:52:06 +0900

    Support for in-cluster lb

---
+++ pkg/api/bgp.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"context"
+)
+
+type BGPNeigh struct {
+	// BGP Neighbor IP address
+	IPAddress string `json:"ipAddress,omitempty"`
+	// Remote AS number
+	RemoteAs int64 `json:"remoteAs,omitempty"`
+}
+
+type BGPGlobalConfig struct {
+	// Local AS number
+	LocalAs int64 `json:"localAs,omitempty"`
+	// BGP Router ID
+	RouterID string `json:"routerId,omitempty"`
+}
+
+type BGPAPI struct {
+	resource string
+	provider string
+	version  string
+	client   *RESTClient
+	APICommonFunc
+}
+
+func newBGPAPI(r *RESTClient) *BGPAPI {
+	return &BGPAPI{
+		resource: "config/bgp",
+		provider: r.provider,
+		version:  r.version,
+		client:   r,
+	}
+}
+
+func (bgpNeighModel *BGPNeigh) GetKeyStruct() LoxiModel {
+	return bgpNeighModel
+}
+
+func (bgpGlobalModel *BGPGlobalConfig) GetKeyStruct() LoxiModel {
+	return bgpGlobalModel
+}
+
+func (b *BGPAPI) CreateGlobalConfig(ctx context.Context, Model LoxiModel) error {
+	resp := b.client.POST(b.resource + "/global").Body(Model).Do(ctx)
+	if resp.err != nil {
+		return resp.err
+	}
+	return nil
+}
+
+func (b *BGPAPI) CreateNeigh(ctx context.Context, Model LoxiModel) error {
+	resp := b.client.POST(b.resource + "/neigh").Body(Model).Do(ctx)
+	if resp.err != nil {
+		return resp.err
+	}
+	return nil
+}
