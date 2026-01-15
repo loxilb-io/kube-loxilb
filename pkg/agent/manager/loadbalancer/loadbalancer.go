8153264ad18d139b98eaff8009d7ef9bfc9fcf10
backguynn <backguyn@netlox.io>
2026-01-15 16:11:35 +0900

    add n2det epselect

--- pkg/agent/manager/loadbalancer/loadbalancer.go
+++ pkg/agent/manager/loadbalancer/loadbalancer.go
@@ -682,6 +682,8 @@ func (m *Manager) addLoadBalancer(svc *corev1.Service) error {
 		epSelect = api.LbSelN2
 	case "n3":
 		epSelect = api.LbSelN3
+	case "n2det":
+		epSelect = api.LbSelN2DET
 	case "rr":
 		epSelect = api.LbSelRr
 	case "roundrobin":
